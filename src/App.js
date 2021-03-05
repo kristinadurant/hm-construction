69e4d36da240c92d51ae3070410d78b03c88160e
Luis Perez <luis@Luiss-iMac.attlocal.net>
2021-03-05 16:16:37 -0500

    created basic file structure

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 import { Header, Footer } from './components';
+import { About, Contact, ErrorPage, Home, Projects, Services} from './pages/index';
 
 function App() {
   return ( 
@@ -10,7 +11,12 @@ function App() {
       <ScrollToTop />
       <Header />
       <Switch>
-        
+      <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/projects" component={Projects} />
+       <Route component={ErrorPage} />
       </Switch>
       <Footer />
     </BrowserRouter>
