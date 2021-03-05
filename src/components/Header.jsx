89242d9311fb2e3e9fdcffa50b8afe0a09013338
kristinadurant <kristinadurant.p@gmail.com>
2021-03-05 15:11:13 -0500

    added navbar on scroll code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Spring } from 'react-spring/renderprops';
 
-const Header = () => {
+const Header = () => {    
+    const [reverse, setReverse] = useState(false);
+    let prevScrollpos = window.pageYOffset;
+
+    window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+      if (prevScrollpos > currentScrollPos) {
+          //scroll up
+        setReverse(false);
+      } else {
+          //scroll down
+        setReverse(true);
+      }
+      prevScrollpos = currentScrollPos;
+    };
+
     return (
-        <header>
-            
+        <Spring from={{marginTop: -50}} to={{marginTop: 0}} reset={true} reverse={reverse}>
+            {props =>
+        <header style={props}>
+            <div className="inner">
+                <Link to='/' className="logo">
+                    HM
+                </Link>
+                <nav>
+                    <ul>
+                        <li><Link to='/carronade-park-floor-plans'>FLOOR PLANS</Link></li>
+                        <li><Link to='/carronade-park-amenities'>AMENITIES</Link></li>
+                        <li><Link to='/carronade-park-gallery'>GALLERY</Link></li>
+                        <li><Link to='/carronade-park-neighborhood'>NEIGHBORHOOD</Link></li>                       
+                        <li><Link to='/carronade-park-schedule-tour'>SCHEDULE A TOUR</Link></li>             
+                    </ul>
+                </nav>
+            </div>
         </header>
+        }
+        </Spring>
     )
 }
 
