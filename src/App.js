c7560443a444ed61992639ff4ff07bcbc1c8dea4
kristinadurant <kristinadurant.p@gmail.com>
2021-03-05 15:04:26 -0500

    installed react-spring

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
+import { Header, Footer } from './components';
 
 function App() {
   return ( 
     <BrowserRouter>
       <ScrollToTop />
+      <Header />
+      <Switch>
+        
+      </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
